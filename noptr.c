da562a9548fdd106f6198bc92128d26ef83dee2c
walt-wood <80364037+walt-wood@users.noreply.github.com>
2022-10-31 11:56:59 -0500

    Noptr does not store prime numbers, can do over 86100

--- noptr.c
+++ noptr.c
@@ -2,47 +2,39 @@
 #include <stdlib.h>
 #include <math.h>
 
-void printList(int, int *);
-short checkPrime(int, int *);
+#pragma region Iterative
+
+void printList(int);
+int isPrime(int);
 
 int main(int argc, char *argv[]) {
 
     int ceil = (argc >= 2) ? atoi(argv[1]) : 5000;
-    int i = 0, j = 0;
-    short isPrime = 0;
-    int arrLen = (ceil > 17) ? (1.25506 * (ceil / log(ceil))) : ( ceil / log(ceil) );// ceil / 4;
-    
-    int *arr = (int *) malloc(sizeof(int) * arrLen + 1);
 
-    for(int k = 2; k <= ceil; k++) {        
-        i = k / 2;
+    printList(ceil);
 
-        isPrime = checkPrime(k, &i);
-
-        if(isPrime == 1) {
-            arr[j] = k;
-            j++;
-        }
-              
-    }
-
-    printList(arrLen, arr);
     return 0;
 }
 
-void printList(int len, int *arr) {
 
-    for(int i = 0; i < len; i++) {
-        if(arr[i]) printf("%5d ", arr[i]);
+void printList(int n) {
+
+    for(int i = 2; i <= n; i++) {
+        if(isPrime(i)) printf("%5d ", i);
     }
 }
 
-short checkPrime(int ceil, int *i) {
+int isPrime(int n) {
 
-    if(*i == 1) return 1;
-    else if(ceil % *i == 0) return 0;
-    else {        
-        (*i)--;
-        checkPrime(ceil, i);
+    if(n <= 1) return 0;
+    if(n <= 3) return 1;
+    if(n % 2 == 0 || n %3 == 0) return 0;
+
+    for(int i = 5; i * i <= n; i += 6) {
+        if(n % i == 0 || n % (i + 2) == 0) return 0;
     }
+
+    return 1;
 }
+
+#pragma endregion Iterative
